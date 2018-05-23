922b213f222216c1e2b92053eb820169b6391ca9
Oscar <oscar101019@gmail.com>
2018-05-23 01:22:42 -0600

    Contactos

---
+++ app/src/main/java/com/example/oscar/a59contactos/ContactAdapter.java
@@ -0,0 +1,42 @@
+package com.example.oscar.a59contactos;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.RecyclerViewHolder> {
+    String[][] data;
+
+    public ContactAdapter(String[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public ContactAdapter.RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_layout,parent,false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ContactAdapter.RecyclerViewHolder holder, int position) {
+        holder.name.setText(data[position][0]);
+        holder.number.setText(data[position][1]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView name,number;
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            name=itemView.findViewById(R.id.name);
+            number=itemView.findViewById(R.id.number);
+        }
+    }
+}
